84346723b33251562f38807e3cbe4ae158aa228f
Víctor Carrión García <victorrcarrionn@Victors-MBP-5.technicolor.net>
2025-06-30 14:36:13 +0200

    soy el que es y el que anda con los que son 2

--- clubdefutbol/src/main/java/modelo/ClienteFactura.java
+++ clubdefutbol/src/main/java/modelo/ClienteFactura.java
@@ -8,6 +8,28 @@ package modelo;
  *
  * @author PCLino
  */
-public class Cliente {
+public class ClienteFactura {
+    private String cif;
+    private String nombre;
+    
+    public ClienteFactura(String cif, String nombre) {
+        this.cif = cif;
+        this.nombre = nombre;
+    }
+
+    public String getCif() {
+        return cif;
+    }
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
